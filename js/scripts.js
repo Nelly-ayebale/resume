051b6b3bf7fb41a91dc9e4cf440241c2e6c4f62d
Nelly-ayebale <ayebalenelly26@gmail.com>
2022-10-13 18:50:23 +0200

    Made some changes to the responsiveness

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,61 @@
-let theme = localStorage.getItem('theme');
+const colorSwitch = document.getElementById('input-color-switch');
+colorSwitch.addEventListener('click', checkMode);
+const theme = localStorage.getItem('theme');
 
-if (theme==null) {
-    setTheme('light');
+if(theme=="dark-mode"){
+    darkModeOn();
 }else{
-    setTheme(theme);
+    darkModeOff();
 }
 
-let themeDots = document.getElementsByClassName('theme-dot');
+function checkMode(){
+    console.log("checking...")
 
-for (let index = 0; index < themeDots.length; index++) {
-    themeDots[index].addEventListener('click', function(){
-        let mode = this.dataset.mode;
-        console.log('Option clicked:', mode);
-        setTheme(mode);
-    })
+    if(colorSwitch.checked){
+        console.log('dark on');
+        darkModeOn();
+    }else{
+        console.log('dark-off');
+        darkModeOff();
+    }
+
+}
+ 
+function darkModeOn(){
+    document.body.classList.add("dark-mode");
+    colorSwitch.checked = true;
+    localStorage.setItem('theme',"dark-mode");
+}
+
+function darkModeOff(){
+    document.body.classList.remove("dark-mode");
+    colorSwitch.checked=false;
+    localStorage.setItem('theme',"light-mode");
     
 }
-function setTheme(mode) {
-    if (mode=='light') {
-        document.getElementById('theme-color').href='./styles/light.css';
-    }else if(mode=='dark'){
-        document.getElementById('theme-color').href='./styles/dark.css';
-    }
-    localStorage.setItem('theme',mode);
-}
+
+const email = document.querySelector('p#email');
+const popup = document.querySelector('.popup');
+const copyBtn = document.getElementById('copy')
+
+copyBtn.addEventListener('click', ()=>{
+   popup.classList.add('active');
+   copyToClipboard();
+});
+
+popup.addEventListener('animationend', ()=>{
+    popup.classList.remove('active');
+});
+
+function copyToClipboard(){
+const textarea = document.createElement("textarea");
+  textarea.setAttribute("readonly", "");
+  textarea.value = email.innerHTML;
+  textarea.style.position = "absolute";
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy");
+  document.body.removeChild(textarea);
+
+};
+
